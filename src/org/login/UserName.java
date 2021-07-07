b95b2e555f373465c9ceaf093781c9b98ad0d5e1
unknown <info.cloudline@gmail.com>
2021-07-07 12:13:39 +0530

    Nisha Password Conmmit

--- src/org/login/UserName.java
+++ src/org/login/UserName.java
@@ -2,13 +2,13 @@ package org.login;
 
 public class UserName {
 	
-public static void userName() {
-System.out.println("User Name");
+public static void password() {
+System.out.println("password");
 
 }
 
 public static void main(String[] args) {
-	userName();
+	password();
 }
 
 }
