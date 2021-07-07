f96957c2a2f90cf52e580ff227ee4c17475e0aa6
unknown <info.cloudline@gmail.com>
2021-07-07 11:54:44 +0530

    Today Update

---
+++ src/org/login/UserName.java
@@ -0,0 +1,12 @@
+package org.login;
+
+public class UserName {
+
+	public static void userName() {
+	System.out.println("User Name");
+
+	}
+	public static void main(String[] args) {
+		userName();
+	}
+}
